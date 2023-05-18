3839e26457e712c3d76b65bac72b3e3524336a70
Axel <a.nix@gmx.de>
2023-05-18 14:51:14 -0400

    prepare for second doc

--- taskpane.js
+++ taskpane.js
@@ -21,6 +21,7 @@ async function run() {
      * Insert your Word code here
      */
 
+	var title=context.DocumentProperties.title;
 	var bookmarks = context.document.body.getRange().getBookmarks();
 	await context.sync();
 	console.log("Bookmarks:");
@@ -28,56 +29,60 @@ async function run() {
 	console.log(bookmarks.value);
 	console.log(bookmarks.value.length);
 
-	var type=context.document.getBookmarkRange("Type");
-	var count=context.document.getBookmarkRange("Count");
-	var appno=context.document.getBookmarkRange("Application");
-	type.load('text');
-	count.load('text');
-	appno.load('text');
-	await context.sync();
 
-	console.log("Type: ",type);
-	console.log(type.text);
-	console.log("Count: ",type);
-	console.log(count.text,parseInt(count.text));
-	console.log(appno);
+	if (title.includes("AssignmentDeclaration")){
+		var type=context.document.getBookmarkRange("Type");
+		var count=context.document.getBookmarkRange("Count");
+		var appno=context.document.getBookmarkRange("Application");
+		type.load('text');
+		count.load('text');
+		appno.load('text');
+		await context.sync();
 	
-	// Set alternatives based on text
-
-	if (type.text.includes("Utility")){
-		var alt1="Option_C"	
-	}
-	if (type.text.includes("Design")){
-		var alt1="Option_D"	
-	}
+		console.log("Type: ",type);
+		console.log(type.text);
+		console.log("Count: ",type);
+		console.log(count.text,parseInt(count.text));
+		console.log(appno);
+		
+		// Set alternatives based on text
 	
-	let i = 0;
-
-	if (parseInt(count.text)>1){
-		var alt2="Option_A"	
-	}
-	else {
-		var alt2="Option_B"	
-	}
-	
-	if (appno.text.includes("PCT")){
-		var alt3="ApplicationType_US"	
-	}
-	else {
-		var alt3="ApplicationType_PCT"	
-	}
-	
-	console.log("Selected alternatives: ",alt1, alt2,alt3);
-	
-	while (i < bookmarks.value.length) {
-		console.log(bookmarks.value[i]);
-		if (bookmarks.value[i].includes(alt1) || bookmarks.value[i].includes(alt2)|| bookmarks.value[i].includes(alt3)) {
-			console.log("Now deleting ",bookmarks.value[i],alt1);
-			var range=context.document.getBookmarkRange(bookmarks.value[i]).delete();
+		if (type.text.includes("Utility")){
+			var alt1="Option_C"	
 		}
-		i++;
-	}	
+		if (type.text.includes("Design")){
+			var alt1="Option_D"	
+		}
+		
+		let i = 0;
+	
+		if (parseInt(count.text)>1){
+			var alt2="Option_A"	
+		}
+		else {
+			var alt2="Option_B"	
+		}
+		
+		if (appno.text.includes("PCT")){
+			var alt3="ApplicationType_US"	
+		}
+		else {
+			var alt3="ApplicationType_PCT"	
+		}
+		
+		console.log("Selected alternatives: ",alt1, alt2,alt3);
+		
+		while (i < bookmarks.value.length) {
+			console.log(bookmarks.value[i]);
+			if (bookmarks.value[i].includes(alt1) || bookmarks.value[i].includes(alt2)|| bookmarks.value[i].includes(alt3)) {
+				console.log("Now deleting ",bookmarks.value[i],alt1);
+				var range=context.document.getBookmarkRange(bookmarks.value[i]).delete();
+			}
+			i++;
+		}	
+	}
 
+	
   // Load objects for display in Script Lab console.
   await context.sync();
 
