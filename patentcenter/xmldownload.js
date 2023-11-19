6c67e088d32306e22e70dfa03732cf4706a6860f
Axel <a.nix@gmx.de>
2023-11-19 10:21:29 -0500

    added url

--- patentcenter/xmldownload.js
+++ patentcenter/xmldownload.js
@@ -1,8 +1,10 @@
 
-const apps = [22, 54, 76, 92, 43, 33];
+const apps = [18501613,18031050,18030644,18030128];
 let i = 0;
 
 while (i < apps.length) {
     console.log(apps[i]);
+    url="https://patentcenter.uspto.gov/retrieval/private/v2/applications/"+apps[i]+"/data"
+    console.log(url);
     i++;
 }
