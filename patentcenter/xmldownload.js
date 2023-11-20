676377fc952621418b751f950b789b6ad60b97e9
Axel <a.nix@gmx.de>
2023-11-19 19:06:22 -0500

    include filesaver

--- patentcenter/xmldownload.js
+++ patentcenter/xmldownload.js
@@ -2,6 +2,11 @@
 const apps = [18501613,18031050,18030644,18030128];
 let i = 0;
 
+var FileSaver = require('FileSaver.js');
+var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+FileSaver.saveAs(blob, "hello world.txt");
+
+
 while (i < apps.length) {
     console.log(apps[i]);
     url="https://patentcenter.uspto.gov/retrieval/private/v2/applications/"+apps[i]+"/data"
